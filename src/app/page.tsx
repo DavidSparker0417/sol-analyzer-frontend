58e2fdd8239d6b167b7726c95ece40098ddc5df6
DavidSparker <davidsparker0417@gmain.com>
2024-08-20 17:35:18 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       {
         fetching
           ? <h1>Fetching ...</h1>
-          : <table>
+          : <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead>
               <tr>
                 <th>when</th>
